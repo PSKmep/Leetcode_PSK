6b1476ef5c7a6904eaec273b23700cce29c6fbec
PSKmep <gomathisri10_bai27@mepcoeng.ac.in>
2026-04-01 16:33:32 +0000

    Sync LeetCode submission Runtime - 13 ms (22.50%), Memory - 46.6 MB (28.51%)

---
+++ my-past-solutions/0224-basic-calculator/solution.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int res = 0;
+        int num = 0;
+        int sign = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                num = 10 * num + (c - '0');
+            } else if (c == '+') {
+                res += sign * num;
+                num = 0;
+                sign = 1;
+            } else if (c == '-') {
+                res += sign * num;
+                num = 0;
+                sign = -1;
+            } else if (c == '(') {
+                stack.push(res);
+                stack.push(sign);
+                res = 0;
+                sign = 1;
+            } else if (c == ')') {
+                res += sign * num;
+                num = 0;
+                res *= stack.pop();
+                res += stack.pop();
+            }
+        }
+        
+        return res + (sign * num);
+    }
+}
+
