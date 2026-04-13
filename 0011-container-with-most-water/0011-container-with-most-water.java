6c2364dc9362d04116549496633cdae2480112ad
PSKmep <gomathisri10_bai27@mepcoeng.ac.in>
2026-04-13 19:52:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,5 +1,28 @@
 class Solution {
-    public int maxArea(int[] height) {
-        
+    public int maxArea(int[] height) 
+    {
+        int l=0;
+        int a,w,h;
+        int i=0;
+        int j=height.length-1;
+        while(i<j)
+        {
+            a=Math.min(height[i],height[j]);
+            w=j-i;
+            h=a*w;
+            if(h>l)
+            {
+                l=h;
+            }
+            if(height[i]<height[j])
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+     return l;   
     }
 }
